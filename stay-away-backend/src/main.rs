6f0780716a3ffbf587312e7b6b6fedc049b23deb
Kristyan Yochev <kris.yochev@gmail.com>
2022-07-26 18:49:05 +0300

    Got a baseic echo ws server running

--- stay-away-backend/src/main.rs
+++ stay-away-backend/src/main.rs
@@ -1,8 +1,22 @@
 use warp::{self, Filter};
+use futures_util::{StreamExt, FutureExt};
 
 #[tokio::main]
 async fn main() {
-    let hello_world = warp::any().map(|| "Hello World");
+    // let hello_world = warp::any().map(|| "Hello World");
 
-    warp::serve(hello_world).run(([0, 0, 0, 0], 8080)).await;
+    let routes = warp::path("echo")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            ws.on_upgrade(|websocket| {
+                let (tx, rx) = websocket.split();
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket wrror: {:?}", e);
+                    }
+                })
+            })
+        });
+
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
 }
