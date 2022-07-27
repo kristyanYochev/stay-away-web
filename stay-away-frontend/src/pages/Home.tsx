d5d84d107b054705af06fb78b9abd46aa6d6167f
Kristyan Yochev <kris.yochev@gmail.com>
2022-07-27 16:30:04 +0300

    Added username UI and controls

--- stay-away-frontend/src/pages/Home.tsx
+++ stay-away-frontend/src/pages/Home.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home: React.FC = () => {
+  const [username, setUsername] = useState('');
+
   return (
     <>
       <h1>Home Page</h1>
       <form>
         <label htmlFor='username'>
           Your username:
-          <input type='text' placeholder='Definetly the thing' id='username'/>
+          <input
+            type='text'
+            placeholder='Definetly the thing'
+            id='username'
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
         </label>
       </form>
     </>
