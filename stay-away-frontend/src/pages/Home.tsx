9f286017ee9091db02166fb6054e32eadac842ad
Kristyan Yochev <kris.yochev@gmail.com>
2022-07-27 16:51:23 +0300

    Added remaining fields with basic form controls

--- stay-away-frontend/src/pages/Home.tsx
+++ stay-away-frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const Home: React.FC = () => {
   const [username, setUsername] = useState('');
+  const [lobbyId, setLobbyId] = useState('');
 
   return (
     <>
@@ -9,12 +10,26 @@ const Home: React.FC = () => {
       <form>
         <label htmlFor='username'>
           Your username:
+          <br />
           <input
             type='text'
             placeholder='Definetly the thing'
             id='username'
             value={username}
             onChange={e => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type='button'>Create lobby</button>
+        or
+        <label htmlFor='lobyId'>
+          <button type='button'>Join an existing lobby</button>
+          <input
+            type='text'
+            placeholder='Lobby Id'
+            id="lobbyId"
+            value={lobbyId}
+            onChange={e => setLobbyId(e.target.value)}
           />
         </label>
       </form>
