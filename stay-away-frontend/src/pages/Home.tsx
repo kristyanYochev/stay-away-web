610cacc981a11301edb95e1b7541e90cb2f0d383
Kristyan Yochev <kris.yochev@gmail.com>
2022-07-27 16:25:44 +0300

    Added username field

--- stay-away-frontend/src/pages/Home.tsx
+++ stay-away-frontend/src/pages/Home.tsx
@@ -4,6 +4,12 @@ const Home: React.FC = () => {
   return (
     <>
       <h1>Home Page</h1>
+      <form>
+        <label htmlFor='username'>
+          Your username:
+          <input type='text' placeholder='Definetly the thing' id='username'/>
+        </label>
+      </form>
     </>
   )
 };
