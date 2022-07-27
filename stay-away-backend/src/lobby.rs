f18210c7ba7d4873db1084ae0a7ebed575e5af14
Kristyan Yochev <kris.yochev@gmail.com>
2022-07-27 22:05:18 +0300

    Added lobby creation for the server side

--- stay-away-backend/src/lobby.rs
+++ stay-away-backend/src/lobby.rs
@@ -1,20 +1,36 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
 use rand::Rng;
 
-pub struct Lobby {
-  id: String,
-}
+pub struct Lobby;
+
+pub type Lobbies = Arc<RwLock<HashMap<String, Lobby>>>;
 
 impl Lobby {
   pub fn new() -> Self {
-    use rand::thread_rng;
-    use rand::distributions::Alphanumeric;
-
-    Self {
-      id: thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(10)
-        .map(char::from)
-        .collect(),
-    }
+    Self
   }
+}
+
+pub async fn generate_id(lobbies: &Lobbies) -> String {
+  let mut id = random_id();
+
+  while lobbies.read().await.contains_key(&id) {
+      id = random_id();
+  }
+
+  id
+}
+
+fn random_id() -> String {
+  use rand::thread_rng;
+  use rand::distributions::Alphanumeric;
+
+  thread_rng()
+    .sample_iter(&Alphanumeric)
+    .take(6)
+    .map(char::from)
+    .collect()
 }
--- stay-away-backend/src/main.rs
+++ stay-away-backend/src/main.rs
@@ -1,10 +1,16 @@
 mod lobby;
 
+use std::convert::Infallible;
+
+use lobby::{Lobbies, Lobby};
 use warp::{self, Filter};
 use futures_util::{StreamExt, FutureExt};
 
 #[tokio::main]
 async fn main() {
+    let lobbies = Lobbies::default();
+    let lobbies = warp::any().map(move || lobbies.clone());
+
     let ws_echo = warp::path("echo")
         .and(warp::ws())
         .map(|ws: warp::ws::Ws| {
@@ -18,9 +24,21 @@ async fn main() {
             })
         });
 
-    let routes = warp::get().and(
-        ws_echo
-    );
+    let create_lobby = warp::path("lobbies")
+        .and(warp::path::end())
+        .and(warp::post())
+        .and(lobbies)
+        .and_then(handle_create_lobby);
+
+    let routes = ws_echo.or(create_lobby);
 
     warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
+}
+
+async fn handle_create_lobby(lobbies: Lobbies) -> Result<impl warp::Reply, Infallible> {
+    let id = lobby::generate_id(&lobbies).await;
+
+    lobbies.write().await.insert(id.clone(), Lobby::new());
+
+    Ok(id)
 }
