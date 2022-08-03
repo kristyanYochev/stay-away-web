94fd88443102ef9f71af0968e365f41ed5c010e8
Kristyan Yochev <kris.yochev@gmail.com>
2022-08-04 00:15:33 +0300

    Added UI for joinin rooms

--- stay-away-frontend/src/pages/Lobby.tsx
+++ stay-away-frontend/src/pages/Lobby.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Lobby: React.FC = () => {
+  const [username, setUsername] = useState("");
+
   const { lobbyId } = useParams();
 
+  const joinRoom = () => {
+    
+  }
+
   return (
-    <h1>Lobby {lobbyId}</h1>
+    <>
+      <h1>Lobby {lobbyId}</h1>
+      <input
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+        placeholder="Your awesome username"
+      />
+      <button onClick={joinRoom}>Join Room</button>
+    </>
   );
 };
 
