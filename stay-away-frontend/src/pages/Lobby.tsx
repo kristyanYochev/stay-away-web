c6b280190bc5f644707b9f5d7f0efcf89c337b21
Kristyan Yochev <kris.yochev@gmail.com>
2022-08-09 20:57:18 +0300

    New users show up for previously joined users

--- stay-away-frontend/src/pages/Lobby.tsx
+++ stay-away-frontend/src/pages/Lobby.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 const Lobby: React.FC = () => {
   const [username, setUsername] = useState("");
+  const [users, setUsers] = useState<string[]>([]);
   const ws = useRef<WebSocket | null>(null);
 
   const { lobbyId } = useParams();
@@ -11,7 +12,12 @@ const Lobby: React.FC = () => {
     const socket = new WebSocket(`ws://localhost:8080/lobbies/${lobbyId}`);
     socket.onopen = () => console.log(`Socket opened to ${lobbyId}`);
     socket.onclose = () => console.log(`Socket to ${lobbyId} is closed`);
-    socket.onmessage = console.log;
+    socket.onmessage = wsMessage => {
+      const message = JSON.parse(wsMessage.data);
+      if (message.event === "UserJoined") {
+        setUsers(users => [...users, message.data.username]);
+      }
+    };
 
     ws.current = socket;
 
@@ -37,6 +43,14 @@ const Lobby: React.FC = () => {
         placeholder="Your awesome username"
       />
       <button onClick={joinRoom}>Join Room</button>
+      <br />
+      <ul>
+        {users.map((user, i) => (
+          <li key={i}>
+            {user}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
