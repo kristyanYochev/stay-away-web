71d57bc9b94c9331a45f445ff0c174137298bea2
Kristyan Yochev <kris.yochev@gmail.com>
2022-08-09 20:48:56 +0300

    Added websocket connections for joining

--- stay-away-frontend/src/pages/Lobby.tsx
+++ stay-away-frontend/src/pages/Lobby.tsx
@@ -3,19 +3,29 @@ import { useParams } from "react-router-dom";
 
 const Lobby: React.FC = () => {
   const [username, setUsername] = useState("");
-  const ws = useRef<WebSocket>(null);
+  const ws = useRef<WebSocket | null>(null);
 
   const { lobbyId } = useParams();
 
   useEffect(() => {
     const socket = new WebSocket(`ws://localhost:8080/lobbies/${lobbyId}`);
+    socket.onopen = () => console.log(`Socket opened to ${lobbyId}`);
+    socket.onclose = () => console.log(`Socket to ${lobbyId} is closed`);
     socket.onmessage = console.log;
 
+    ws.current = socket;
+
     return () => socket.close();
-  }, []);
+  }, [lobbyId]);
 
   const joinRoom = () => {
-    
+    console.log(ws.current);
+    ws.current?.send(JSON.stringify({
+      event: "Join",
+      data: {
+        username
+      }
+    }));
   }
 
   return (
