4964800142191118a3d5b72ce9a455b592875c07
Kristyan Yochev <kris.yochev@gmail.com>
2022-08-21 18:08:01 +0300

    Created a few type aliases for readability

--- stay-away-frontend/src/stayAwayApi/index.ts
+++ stay-away-frontend/src/stayAwayApi/index.ts
@@ -1,15 +1,19 @@
 import { useEffect, useRef } from "react";
 import { ServerEventMap } from "./serverEvents";
 
+type ServerEventType = keyof ServerEventMap;
+type ServerEvent<E extends ServerEventType> = ServerEventMap[E];
+type ServerEventListener<E extends ServerEventType> = (event: ServerEvent<E>) => any;
+
 interface StayAwayAPI {
-  on<E extends keyof ServerEventMap>(eventType: E, listener: (event: ServerEventMap[E]) => any): void;
+  on<E extends ServerEventType>(eventType: E, listener: ServerEventListener<E>): void;
 }
 
 type EventListeners = {
-  [E in keyof ServerEventMap]: ((event: ServerEventMap[E]) => any)[];
+  [E in ServerEventType]: ((event: ServerEventMap[E]) => any)[];
 };
 
-interface WebsocketMessage<E extends keyof ServerEventMap> {
+interface WebsocketMessage<E extends ServerEventType> {
   event: E,
   data: ServerEventMap[E],
 }
@@ -24,11 +28,11 @@ export default function useStayAway(lobbyId: string): StayAwayAPI {
   useEffect(() => {
     const socket = new WebSocket(`ws://localhost:8080/lobbies/${lobbyId}`);
 
-    const emit = <E extends keyof ServerEventMap>(eventType: E, event: ServerEventMap[E]) => {
+    const emit = <E extends ServerEventType>(eventType: E, event: ServerEventMap[E]) => {
       eventListeners.current[eventType].forEach(handle => handle(event));
     };
   
-    const handleWebsocketMessage = <E extends keyof ServerEventMap>(msg: MessageEvent<any>) => {
+    const handleWebsocketMessage = <E extends ServerEventType>(msg: MessageEvent<any>) => {
       console.log(`Received message: ${msg}`);
       const {event: eventType, data: event} = JSON.parse(msg.data) as WebsocketMessage<E>;
       emit(eventType, event);
@@ -41,7 +45,7 @@ export default function useStayAway(lobbyId: string): StayAwayAPI {
     return () => socket.close();
   }, [lobbyId]);
 
-  const subscribe = <E extends keyof ServerEventMap>(eventType: E, listener: (event: ServerEventMap[E]) => any) => {
+  const subscribe = <E extends ServerEventType>(eventType: E, listener: (event: ServerEventMap[E]) => any) => {
     eventListeners.current[eventType].push(listener);
   }
 
