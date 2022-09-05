91d82086049e220244b2a2faaf29e5de47c5c03b
Kristyan Yochev <kris.yochev@gmail.com>
2022-09-06 01:04:22 +0300

    Patched import

--- stay-away-frontend/src/pages/Lobby.tsx
+++ stay-away-frontend/src/pages/Lobby.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import useStayAway, { StayAwayProvider } from "../stayAwayApi";
+import { useStayAway, StayAwayProvider } from "../stayAwayApi/index";
 
 const Lobby = () => {
   const { lobbyId } = useParams();
