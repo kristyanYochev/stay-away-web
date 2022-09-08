ac6c98b9ea5c3b59e7f7a0772a46de5d83ff9e30
Kristyan Yochev <kris.yochev@gmail.com>
2022-09-08 16:47:01 +0300

    Updated frontend API to reflect protocol changes

--- stay-away-frontend/src/stayAwayApi/serverEvents.ts
+++ stay-away-frontend/src/stayAwayApi/serverEvents.ts
@@ -1,10 +1,20 @@
 interface UsersUpdatedEvent {
-  users: string[];
+  users: User[];
+}
+
+export type User = {
+  username: string;
+  id: number;
 }
 
 interface ErrorEvent {}
 
+interface WelcomeEvent {
+  id: number;
+}
+
 export interface ServerEventMap {
   "UsersUpdated": UsersUpdatedEvent;
   "Error": ErrorEvent;
+  "Welcome": WelcomeEvent;
 }
